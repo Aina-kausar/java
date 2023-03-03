d55aad9ca880f0c9a28fcd0beadff6c2ee45723a
Aina-kausar <126906849+Aina-kausar@users.noreply.github.com>
2023-03-04 00:53:06 +0530

    Add files via upload

---
+++ Mul.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Mul {
+    public static void main(String args[]) {
+        System.out.println("Enter two numbers:");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int product = a * b;
+        System.out.println(product);
+    }
+
+}
