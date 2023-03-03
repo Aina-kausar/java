15246db9200a3d0ffa8a037692d3136e253a19ec
Aina-kausar <126906849+Aina-kausar@users.noreply.github.com>
2023-03-04 00:42:04 +0530

    Add files via upload

---
+++ Expressions.java
@@ -0,0 +1,12 @@
+public class Expressions {
+    public static void main(String args[]) {
+        int a = (-5) + 8 * 6;
+        int b = (55 + 9) % 9;
+        float c = 20 + (-3) * 5 / 8;
+        float d = 5 + 15 / (3 * 2) - 8 % 3;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
